df27de1c33abd5821124b3a59c89305e8db0246d
Dario Ignacio Mansilla <mansilladario94@gmail.com>
2023-06-05 12:24:31 -0300

    Se añade renderizado condicional del signUp en funcion de si es guest u host

--- js/signup.js
+++ js/signup.js
@@ -16,9 +16,116 @@ function deleteFromLocalStorage(name) {
 	localStorage.removeItem(name);
 }
 
+// !Logica para renderizar los signup- ----------------------------------------------
+function createSignUp(mode) {
+	const paramsGeneral = {
+		host: {
+			title: 'Regístrate como anfitrión',
+		},
+		guest: {
+			title: 'Regístrate como huésped',
+		},
+	};
+	const params = paramsGeneral[mode];
+	const htmlString = `
+    <h2>${params.title}</h2>
+    <div class="col-6 d-flex flex-column align-content-center gap-2">
+						<!-- email -->
+						<div class="form-floating mb-3">
+							<input
+								value="email@gmail.com"
+								required
+								type="email"
+								class="form-control"
+								maxlength="64"
+								minlength="4"
+								name="emailInput"
+								id="emailInput"
+								placeholder="Ingrese su email" />
+							<label for="emailInput">Nombre de usuario</label>
+						</div>
+						<!-- nombre -->
+						<div class="form-floating mb-3">
+							<input
+								value="nombre test"
+								required
+								type="text"
+								class="form-control"
+								maxlength="64"
+								minlength="4"
+								id="nameInput"
+								name="nameInput"
+								placeholder="Nombre de usuario" />
+							<label for="nameInput">Ingrese su nombre completo</label>
+						</div>
+						<!-- apellido -->
+						<div class="form-floating mb-3">
+							<input
+								value="nombre test"
+								required
+								type="text"
+								class="form-control"
+								maxlength="64"
+								minlength="4"
+								id="lastNameInput"
+								name="lastNameInput"
+								placeholder="Nombre de usuario" />
+							<label for="lastNameInput">Ingrese su apellido</label>
+						</div>
+						<!-- contraseña -->
+						<div class="form-floating mb-3">
+							<input
+								value="contrasena"
+								required
+								type="password"
+								class="form-control"
+								maxlength="256"
+								minlength="8"
+								id="passwordInput"
+								name="passwordInput"
+								placeholder="Ingrese su contraseña" />
+							<label for="passwordInput">Ingrese su contraseña</label>
+						</div>
+						<!-- numero celular (optativo) -->
+						<div class="form-floating">
+							<input
+								value="3813869543"
+								type="number"
+								class="form-control"
+								pattern="[0-9]{10}"
+								id="phoneInput"
+								name="phoneInput"
+								placeholder="Numero de celular" />
+							<label for="phoneInput">Numero de celular</label>
+						</div>
+
+						<input type="submit" class="btn btn-primary" value="Registrarme" />
+					</div>
+    `;
+	const $form = document.createElement('form');
+	$form.innerHTML = htmlString;
+	$form.setAttribute('id', 'newUserRegister');
+	$form.classList.add('row');
+	return $form;
+}
+
+// Handler del click para registrar usuario
+function handleClickSignUp(e) {
+	// Defino si el boton fue de guest o host para el signup
+	const mode = e.target.id === 'hostSignUp' ? 'host' : 'guest';
+	// Limpio el elemento padre donde renderizar
+	document.querySelector('#signupContainer').innerHTML = '';
+	// Inserto el elemento
+	document.querySelector('#signupContainer').appendChild(createSignUp(mode));
+}
+// Controladores de los botones de registro
+document.querySelector('#guestSignUp').onclick = handleClickSignUp;
+document.querySelector('#hostSignUp').onclick = handleClickSignUp;
+
+// !----------------Logica de signUP
 // Obtengo en formulario del HTML
 
-const $formSignUp = document.querySelector('#newUserRegister');
+// const $formSignUp = document.querySelector('#newUserRegister');
 
 // Manejador de registro de usuarios
 
@@ -51,7 +158,7 @@ function handleNewUserRegister(e) {
 }
 
 // Controlador eventos al submit del formSignUp
-$formSignUp.addEventListener('submit', handleNewUserRegister);
+// $formSignUp.addEventListener('submit', handleNewUserRegister);
 
 // ! Logica del login ----------------------------------------------------------------------------------
 // Obtengo el form del login
