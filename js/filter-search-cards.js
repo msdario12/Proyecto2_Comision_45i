b41366b5d00433eb003dd25eb0de5fafbd28a1d6
Dario Ignacio Mansilla <mansilladario94@gmail.com>
2023-06-08 10:52:51 -0300

    Se corrige src de scripts de filePond que estaban offline

--- js/filter-search-cards.js
+++ js/filter-search-cards.js
@@ -1,1 +1,8 @@
 // Logica encargada de filtrar las cards y realizar busquedas
+
+// Obtener el input de busqueda por titulo
+// Leer el valor del input cuando se apriete enter o el boton de buscar
+// Leer del localStorage solo las publicaciones que incluyan la busqueda
+// Guardar ese array que cumple en una variable
+// Limpiar las cards anteriores que se esten mostrando
+// Renderizar las nuevas publicaciones en base a la busqueda.
