fa4a27f67bdcdc3da1f6aa87f3df8b2ace32051a
Dario Ignacio Mansilla <mansilladario94@gmail.com>
2023-06-08 11:15:02 -0300

    Se agrega la logica para realizar busquedas y renderizar solo aquellas que cumplan

--- js/filter-search-cards.js
+++ js/filter-search-cards.js
@@ -1,8 +1,22 @@
 // Logica encargada de filtrar las cards y realizar busquedas
-
+// Obtener las publicaciones del localStorage
+let cardsToFilter = getFromLocalStorage('accommodationDB');
 // Obtener el input de busqueda por titulo
-// Leer el valor del input cuando se apriete enter o el boton de buscar
-// Leer del localStorage solo las publicaciones que incluyan la busqueda
-// Guardar ese array que cumple en una variable
-// Limpiar las cards anteriores que se esten mostrando
-// Renderizar las nuevas publicaciones en base a la busqueda.
+const $inputSearch = document.querySelector('#searchInput');
+// Controlador a ejecutarse con el keyUp
+function handleKeyUpInputSearch(e) {
+	// Leer el valor del input cuando se apriete enter o el boton de buscar
+	if (e.key === 'Enter') {
+		const searchParam = String(e.target.value).toLowerCase();
+		// Leer del localStorage solo las publicaciones que incluyan la búsqueda
+		// Guardar ese array que cumple en una variable
+		const filteredCars = cardsToFilter.filter((card) => {
+			return card.accommodationTitle.toLowerCase().includes(searchParam);
+		});
+		// Limpiar las cards anteriores que se esten mostrando
+		// Renderizar las nuevas publicaciones en base a la busqueda.
+
+		renderCardList(filteredCars);
+	}
+}
+$inputSearch.addEventListener('keyup', handleKeyUpInputSearch);
