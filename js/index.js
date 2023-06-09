bafc9958d6a8ef51dd0f2fbd9d3af74d9719d8ae
Julian Simon <juedsay@gmail.com>
2023-06-09 02:37:49 -0300

    se agregregó barra de busqueda para generar reserva al home

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// Para campo de busqueda de ubicaciónes, utilizo Typeahead:
+$(document).ready(function() {
+    var locations = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        // Aquí puedes agregar tus ubicaciones recomendadas
+        local: ['Ciudad A', 'Ciudad B', 'Ciudad C', 'Ciudad D']
+    });
+
+    $('#searchbox').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    }, {
+        name: 'locations',
+        source: locations
+    });
+});
